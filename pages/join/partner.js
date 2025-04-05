f287937f3bfd0dec1c1c0e8a9b7618652e20d1d6
unknown <aaradhya.korde@gmail.com>
2025-04-06 01:20:09 +0530

    integratino

--- pages/join/partner.js
+++ pages/join/partner.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { db } from '../lib/firebase';
+import { db } from '../../lib/firebase';
 import { collection, addDoc } from 'firebase/firestore';
 import { useRouter } from 'next/router';
 
